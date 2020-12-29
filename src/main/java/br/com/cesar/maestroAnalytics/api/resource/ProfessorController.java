07dac10430b07e2eff1c9be5fcb7ae09a266c39c
Rayssa Neder <rayssafreitas13@gmail.com>
2020-12-29 03:12:01 -0200

    Implementando DELETE de Disciplina, Instituição, pessoa e professor

--- src/main/java/br/com/cesar/maestroAnalytics/api/resource/ProfessorController.java
+++ src/main/java/br/com/cesar/maestroAnalytics/api/resource/ProfessorController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -27,5 +31,11 @@ public class ProfessorController {
 	public List<Professor> listar() {
 		return professoresRepository.findAll();
 	}
+	
+	 @DeleteMapping("/{codigo}")
+	  @ResponseStatus(HttpStatus.NO_CONTENT)
+	  public void remover(@PathVariable Long codigo) {
+		 professoresRepository.delete(codigo);
+	  }
 
 }
